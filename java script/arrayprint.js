0757ba95faeecd564c1c8b77a601926e5a8e36ac
Vignesha U G <143516355+Vignesha0408@users.noreply.github.com>
2026-03-12 09:38:56 +0530

    Add files via upload

---
+++ java script/arrayprint.js
@@ -0,0 +1,15 @@
+
+//funnn clllalled itself
+//rec logic 
+//base connndition whenever we know the outpot of smallert iteretion in fgiven problen that we will mention in base condition ig we aer not mention avt base condition we get error called stact overflow 
+// recursive call ; these fun call is lused to call the same function after sertain modif,, to recall same function we=ith diff parameters.
+let arr=[1,2,3,4,5]
+
+function printArr(arr)
+{
+    for(let i=0;i<arr.length;i++)
+        console.log(arr[i])
+
+}
+
+printArr(arr)
